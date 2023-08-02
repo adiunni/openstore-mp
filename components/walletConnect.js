617bbf4d033b3f4b79d54ce4294b31168077b4c2
Aditya Unnikrishnan <adiunni@hotmail.com>
2023-08-02 17:54:10 +0530

    Package json update and wallet providers remove update

--- components/walletConnect.js
+++ components/walletConnect.js
@@ -1,27 +1,9 @@
 import Web3Modal from "web3modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import Portis from "@portis/web3";
-import Authereum from "authereum";
 import { ethers } from "ethers";
 
 const providerOptions = {
-  walletConnect: {
-    package: WalletConnectProvider,
-    options: {
-      infuraId: "cfc0ba400dcf4a2ca14edff9f68620fd",
-    },
-  },
   binancechainwallet: {
     package: true,
-  },
-  portis: {
-    package: Portis,
-    options: {
-      id: "0d3049b3-d68a-48ca-a9a7-68b31319729a",
-    },
-  },
-  authereum: {
-    package: Authereum,
   },
 };
 
